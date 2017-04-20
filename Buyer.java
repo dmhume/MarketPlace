389554eb80af9e4effcfa4ee43673ffb86cc35a5
Yonghoon <yp21@indiana.edu>
2017-04-20 18:38:51 -0400

    Codes of Buyer class

--- Buyer.java
+++ Buyer.java
@@ -9,7 +9,9 @@ public class Buyer {
 	/**
 	 * Constructor
 	 */
-	public Buyer(int id, String pw, String email, String name) {}
+	public Buyer(int id, String pw, String email, String name) {
+		account = new Account(id, pw, email, name);
+	}
 	
 
 }
