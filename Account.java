43fa68208814d7c1eda2ecd622abb226ea51cb2d
Yonghoon <yp21@indiana.edu>
2017-04-20 18:35:18 -0400

    Codes for Account class methods. but need to discuss the result the format of getHistory() and getAccountInformation()

--- Account.java
+++ Account.java
@@ -6,7 +6,7 @@ public class Account {
 	/**
 	 * Instance fields (Needs to be saved)
 	 */
-	public final int ID;
+	public int ID;
 	private String password;
 	private String emailAddress;
 	private String name;
@@ -15,66 +15,98 @@ public class Account {
 	/**
 	 * Constructor
 	 */
-	public Account(int id, String pw, String email, String name) {
-		
+	public Account(int id, String pw, String email, String nm) {
 		ID = id;
-		
-		
+		password = pw;
+		emailAddress = email;
+		name = nm;
+		history = new ArrayList<Item>();
 	}
 	
 	/**
 	 * Returns ID
-	 * @return String ID number 
+	 * @return Integer ID number 
 	 */
-	public String getID() {}
+	public int getID() {
+		return ID;
+	}
 	
 	/**
 	 * Returns name
 	 * @return String name
 	 */
-	public String getName() {}
+	public String getName() {
+		return name;
+	}
 	
 	/**
 	 * Returns password
 	 * @return String password
 	 */
-	public String getPassword() {}
+	public String getPassword() {
+		return password;
+	}
 	
 	/**
 	 * Replaces ID with given new ID
-	 * @param id Sting new ID
+	 * @param id Integer new ID
 	 */
-	public void setID(String newID) {}
+	public void setID(int newID) {
+		ID = newID;
+	}
 	
 	/**
 	 * Replaces name with given new name
 	 * @param newName String new name
 	 */
-	public void setName(String newName) {}
+	public void setName(String newName) {
+		name = newName;
+	}
 	
 	/**
 	 * Replaces password with given new password
 	 * @param newPass String new password
 	 */
-	public void setPassword(String newPass) {}
+	public void setPassword(String newPass) {
+		password = newPass;
+	}
 	
 	/**
 	 * Returns the history of purchased items for a buyer or of sold items for a seller
 	 * @return String history of items purchased or sold
 	 */
-	public String getHistory() {}
+	public String getHistory() {
+		String result = "";
+		for(Item item : history) {
+			result += item.getName() + ", ";
+		}
+		return result;
+	}
 	
 	/**
 	 * Adds purchased item to history ArrayList
 	 * @param purchasedItem Item object
 	 */
-	public void addHistory(Item purchasedItem) {}
+	public void addHistory(Item purchasedItem) {
+		history.add(purchasedItem);
+	}
 	
 	/**
-	 * Returns all of account information as String
-	 * @return String account information
+	 * Prints all of account information as String
 	 */
-	public String getAccountInformation() {}
+	public void getAccountInformation() {
+		System.out.printf("%-15s%20d\n", "ID: ", ID);
+		System.out.printf("%-15s%20s\n", "Password: ", password);
+		System.out.printf("%-15s%20s\n", "Email Address: ", emailAddress);
+		System.out.printf("%-15s%20s\n", "Name: ", name);
+	}
 	
-
+	// test
+	public static void main(String[] args) {
+		
+		Account test = new Account(111, "abc", "abc@ac.com", "Ryan");
+		test.getAccountInformation();
+		
+	}
+	
 }
