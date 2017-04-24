43d2ec07a1d9a43574858945eeac54261494905c
Yonghoon <yp21@indiana.edu>
2017-04-24 05:06:44 -0400

    Seller class update except updatefile method

--- Seller.java
+++ Seller.java
@@ -1,6 +1,9 @@
 
 import java.util.ArrayList;
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Seller {
 	
@@ -15,43 +18,84 @@ public class Seller {
 	
 	/**
 	 * Constructor
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
 	 */
-	public Seller(int id, String pw, String email, String name) {
+	public Seller(int initial, int id, String pw, String email, String name) throws ClassNotFoundException, IOException {
 		account = new Account(id, pw, email, name);
-		inventories = new Inventory("fileName");
+		// Creates the text file that contains inventory items
+		String filename = "sellerfile.txt";
+		PrintWriter output = new PrintWriter(filename);
+		output.close();
+		inventories = new Inventory(filename);
+		initialID = initial;
+		inventory = new ArrayList<Item>();
+		savedFile = new File(filename);
 	}
 	
 	/**
 	 * Adds a given item to Inventory class object
 	 * @param item Item class object
 	 */
-	public void addItem(Item item) {}
+	public void addItem(Item item) {
+		inventories.addItem(item);
+	}
 	
 	/**
 	 * Removes given item from Inventory class and inventory ArrayList of Seller class
 	 * @param item Item class object
 	 */
-	public void removeItem(Item item) {}
+	public void removeItem(Item item) {
+		inventories.deleteItem(item);
+		inventory.remove(item);
+	}
 	
 	/**
-	 * Changes the information of item with given info. Based on the parameter, parameter 
+	 * Changes the information of item with given info. Based on the parameter 'num', parameter 'info'
 	 * needs to be changed to another type such as double for price 
 	 * @param info String value of information
 	 */
-	public void changeItemInfo(String info) {}
+	public void changeItemInfo(int num, String itemName, String info) {
+		// 1: name/ 2: itemNumber/ 3: description/ 4: quantity/ 5: price
+		for (Item item : inventory) {
+			if (itemName.equals(item.getName())) {
+				//name
+				if (num == 1) {
+					item.setName(info);
+				}
+				else if (num == 2) {
+					item.setItemNumber(Integer.parseInt(info));
+				}
+				else if (num == 3) {
+					item.setDescription(info);
+				}
+				else if (num == 4) {
+					item.setQuantity(Integer.parseInt(info));
+				}
+				else if (num == 5) {
+					item.SetPrice(Double.parseDouble(info));
+				}
+			}//if
+		}//for
+	}
 	
 	/**
 	 * Returns the notification message that the purchased item has shipped
 	 * @param item Item class object
 	 * @return String notification message
 	 */
-	public String notifyBuyer(Item item) {}
+	public String notifyBuyer(Item item) {
+		String result = item.getName() + " has shipped!";
+		return result;
+	}
 	
 	/**
 	 * Adds a given item to ArrayList of inventory of Seller
 	 * @param item Item class objecct
 	 */
-	public void addToInventory(Item item) {}
+	public void addToInventory(Item item) {
+		inventory.add(item);
+	}
 	
 	/**
 	 * Updates all of inventory information needed and saves it as a text file
